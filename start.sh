fd5e7075e2121294224e3bb08a55f5275a42269d
Tanujairam <88611197+Tanujairam123@users.noreply.github.com>
2022-04-15 14:01:08 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Rex-filter-bot
 fi
-cd /EvaMaria
+cd /Rex-filter-bot
 pip3 install -U -r requirements.txt
 echo "Starting rex bot 🔥🔥🔥.........."
 python3 bot.py
